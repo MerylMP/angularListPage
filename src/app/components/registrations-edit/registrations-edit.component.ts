48ccf76d8205e97025f9b8e6157309c23ad2cfca
MerylMP <maria.mphob@gmail.com>
2019-12-06 21:25:05 +0100

    implementing contact modification functionality

--- src/app/components/registrations-edit/registrations-edit.component.ts
+++ src/app/components/registrations-edit/registrations-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonService } from 'src/app/services/person.service';
+import { Person } from 'src/app/classes/Person';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-registrations-edit',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationsEditComponent implements OnInit {
 
-  constructor() { }
+  private index: string;
+  private contactRegistration: Person;
+  private name: string;
+  private surname: string;
+  private age: number;
+  private dni: string;
+  private dateOfBirth: Date;
+  private favouriteColor: string;
+  private gender: string;
+  private notes: string;
+
+
+  constructor(
+    private personService: PersonService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.index = this.activatedRoute.snapshot.paramMap.get('position');
+    this.contactRegistration = this.personService.getContact(this.index);
   }
 
+
+  updateChanges() {
+    this.personService.updateContacts(this.index, this.contactRegistration);
+    this.router.navigateByUrl('contactsList');
+  }
 }
